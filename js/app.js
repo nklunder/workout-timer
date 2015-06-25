0d4bab7c9d4ddf3e1e46925658e411704bff51ee
Nicholas Klunder <nklunder@gmail.com>
2015-06-25 07:27:30 -0700

    refactor JS so it makes more sense

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 function startCountdown(seconds) {
   seconds = seconds * 1000; // convert seconds into millisecons
-  var options = {
-    color: '#FCB03C',
-    strokeWidth: 10,
-    trailWidth: 15,
-    duration: seconds,
-    text: {
-      value: '0'
-    },
-    step: function(state, bar) {
-      bar.setText((bar.value() * (this.duration / 1000)).toFixed(0));
-    }
-  };
-
-  var circle = new ProgressBar.Circle('#display', options);
-
   circle.set(1);
-  circle.animate(0, function () { circle.setText("Done!");});
+  circle.animate(0, { duration: seconds }, function() {
+    circle.setText("Done!");
+  });
 }
 
 var repsInput = document.getElementById("reps");
 var repsBtn = document.getElementById("reps-button");
 
-repsBtn.addEventListener("click", function () {
+repsBtn.addEventListener("click", function() {
   startCountdown(repsInput.value);
+});
+
+var circle = new ProgressBar.Circle('#display', {
+  color: '#FCB03C',
+  strokeWidth: 10,
+  trailWidth: 15,
+  fill: "lime",
+  text: {
+    value: '0'
+  },
+  step: function(state, bar) {
+    bar.setText((bar.value() * (this.duration / 1000)).toFixed(0));
+  }
 });
