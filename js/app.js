c621d1cc10e37a070af114ec629b5e9ab4c8c36a
Nicholas Klunder <nklunder@gmail.com>
2015-06-25 23:59:26 -0700

    wrap inputs in form for better semantics, fix timer text

--- js/app.js
+++ js/app.js
@@ -1,27 +1,32 @@
 function startCountdown(seconds) {
   seconds = seconds * 1000; // convert seconds into millisecons
   circle.set(1);
-  circle.animate(0, { duration: seconds }, function() {
+  circle.animate(0, {
+    duration: seconds,
+    step: function(state, bar) {
+        bar.setText((bar.value() * (this.duration / 1000)).toFixed(0));
+      }
+    },
+    function() {
     circle.setText("Done!");
   });
 }
 
-var repsInput = document.getElementById("reps");
-var repsBtn = document.getElementById("reps-button");
-
-repsBtn.addEventListener("click", function() {
-  startCountdown(repsInput.value);
-});
-
 var circle = new ProgressBar.Circle('#display', {
+  fill: "#DADADA",
   color: '#FCB03C',
-  strokeWidth: 10,
-  trailWidth: 15,
-  fill: "lime",
+  strokeWidth: 4,
+  trailColor: "#C1C1C1",
+  trailWidth: 2,
   text: {
     value: '0'
-  },
-  step: function(state, bar) {
-    bar.setText((bar.value() * (this.duration / 1000)).toFixed(0));
   }
+});
+
+var repsForm = document.getElementById("reps-form");
+repsForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  var repsInput = document.getElementById("reps-input");
+  alert(repsInput.value);
+  startCountdown(repsInput.value);
 });
